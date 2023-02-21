10c85df8273dbdde417906dbe30f6ccaafb4c80d
SUDESHGOWDQ <112839296+SUDESHGOWDQ@users.noreply.github.com>
2023-02-21 22:46:30 +0530

    up

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-let b = document.getElementById("btn13")
-b.addEventListener('click',()=>{
-    const element = document.getElementById("cards");
-    element.remove();
+let a = document.getElementById('btn2')
+a.addEventListener('click', ()=>{
+    document.body.innerHTML+=`<div id="div11">
+    <form action="#" onsubmit="fetch()">
+    <label for="name">Name</label>
+    <input type="text" id="name">
+     <br>
+     <label for="phno">Phno</label>
+     <input type="tel"  id="phno">
+     <br>
+     <label for="Email">Email</label>
+     <input type="email"  id="Email">
+     <br>
+     <label for="Password">Password</label>
+     <input type="password"  id="Password">
+     <br>
+     <input type="submit" value="submit">
+     <input type="reset" value="Reset">
+    </form>
+    </div>`
 })
 
+function fetch(){
+    let name = document.getElementById('name').value
+    let phno = document.getElementById('phno').value
+    let Email = document.getElementById('Email').value
+    let password = document.getElementById('Password').value
+   let body= document.body
+   let div = document.createElement('div')
+   div.innerHTML+=`<table border="1px">
+   <tr>
+   <td> ${( name)}</td>
+   <td>${(phno)}</td>
+   <td>${(Email)}</td>
+   <td>${(password)}</td>
+   </tr>
+   </table>`
+   body.appendChild(div)
+   
+
+}
+
+
+
+
+
+
+let b = document.getElementById('btn3')
+b.addEventListener('click', () =>{
+    const element = document.getElementById('cards')
+    element.remove()
+})
 
 
